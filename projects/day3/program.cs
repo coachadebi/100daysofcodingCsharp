e5074609cef28db37dc2882a4feac740f463576a
coachadebi <adamsbroda@gmail.com>
2021-01-08 05:49:14 +0000

    added program.cs and program.exe files to day3 directory

---
+++ projects/day3/program.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program{
+
+    static void Main(string[] args){
+
+	byte aByte = 65;
+    	short aShort = -2000;
+    	int anInt = 587664;
+    	long aLong = 2765843;
+	float aFloat = 72.81f;
+	double aDouble = -87678.878;
+	decimal aDecimal = 637828863.80m;
+	char aChar = 'B';
+	bool aBoolean = true;
+	string aString = "C Sharp";
+	const int ConstVar = -58221;
+
+	Console.WriteLine(aByte);
+	Console.WriteLine(aShort);
+	Console.WriteLine("{0} {1} {2} {3}", anInt, aFloat, aLong, aDouble);
+	Console.WriteLine($"{aDecimal} {aChar} {aBoolean} {aString} {ConstVar}");
+
+	Console.WriteLine($"{float.MinValue} {float.MaxValue}");
+	Console.WriteLine("{0} {1}", short.MinValue, short.MaxValue);
+
+        }
+}
