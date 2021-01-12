bc8a31fc23e94b3eb1c12b1935ed22b985e9341e
coachadebi <adamsbroda@gmail.com>
2021-01-12 15:34:47 +0000

    added challenge day 7 project to projects folder

---
+++ projects/day7/string_concat.cs
@@ -0,0 +1,15 @@
+using System;
+
+class StringConcat{
+
+    static void Main(string[] args){
+
+	string firstName, lastName, fullName;
+	firstName = "John ";
+	lastName = "Johnson";
+	fullName = firstName + lastName;
+
+	Console.WriteLine("Your full name is: " + fullName);
+
+    }
+}
