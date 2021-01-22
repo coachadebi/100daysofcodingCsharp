25b6b61c247ecc60e5986bfec9c86d50d8aa5540
coachadebi <adamsbroda@gmail.com>
2021-01-22 16:43:25 +0000

    added challege day 13 project to projects folder

---
+++ projects/day13/strings.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Strings{
+
+    static void Main(string[] args){
+
+	string firstName = "Peter";
+	string lastName = "Peterson";
+
+	string fullName = firstName + " " + lastName;
+
+	Console.WriteLine(fullName);
+
+    }
+}
